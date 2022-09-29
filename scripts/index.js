19684646e1686b8915d30aa0536db3cad7072e10
MaxSavostyanov <maxsavostyanov34@gmail.com>
2022-09-29 11:07:35 +0300

    feature: валидация строки ввода email

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,42 @@
-const submitButtonsList = document.querySelectorAll('.write-us__button');
+const buttonsSubmitList = document.querySelectorAll('.write-us__button');
+const emailsList = document.querySelectorAll('.write-us__input');
+console.log(emailsList);
 
-function setSubmitButtonText() {
-  if (window.innerWidth <= 750) {
-    submitButtonsList.forEach(item => item.textContent = '✌️');
-  } else {
-    submitButtonsList.forEach(item => item.textContent = 'Отремонтируйте камеру ✌️');
+const buttonSubmitText = {
+  dekstop: 'Отремонтируйте камеру ✌️',
+  mobile: '✌️',
+}
+
+const buttonSubmitTextCheck = {
+  dekstop: 'Круто, спасибо за доверие!',
+  mobile: '✔',
+}
+
+function setButtonText(button, text) {
+  if (window.innerWidth <= 750 && button.textContent !== text.mobile) {
+    button.textContent = text.mobile;
+  }
+
+  if (window.innerWidth > 750 && button.textContent !== text.dekstop) {
+    button.textContent = text.dekstop;
   }
 }
 
-window.addEventListener('resize', setSubmitButtonText)
+function checkEmail (email) {
+  if (email.checkValidity()) {
+    setButtonText(email.nextElementSibling, buttonSubmitTextCheck);
+  } else {
+    setButtonText(email.nextElementSibling, buttonSubmitText);
+  }
+}
 
-setSubmitButtonText();
+window.addEventListener('resize', () => {
+  emailsList.forEach(email => checkEmail(email));
+})
+
+emailsList.forEach(email => email.addEventListener('input', (event) => checkEmail(event.target)));
+
+buttonsSubmitList.forEach(button => {
+  setButtonText(button, buttonSubmitText);
+  button.disabled = true;
+});
